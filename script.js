11f9ae1edaf6f8875bd30098a914d841791818c0
Logan <loganvk18@gmail.com>
2021-11-05 17:03:07 +0530

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { startConfetti,stopConfetti,removeConfetti } from "./confetti.js";
+
 const playerScoreEl = document.getElementById('playerScore');
 const playerChoiceEl = document.getElementById('playerChoice');
 const computerScoreEl = document.getElementById('computerScore');
@@ -16,7 +18,7 @@ const computerScissors = document.getElementById('computerScissors');
 const computerLizard = document.getElementById('computerLizard');
 const computerSpock = document.getElementById('computerSpock');
 
-const allGameIcons = document.querySelector(".far");
+const allGameIcons = document.querySelectorAll(".far");
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -25,7 +27,130 @@ const choices = {
   lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
+let playerScore =0;
+let computerScore =0;
+let computerChoice ='';
+
+function resetSelected(){
+  
+  allGameIcons.forEach((icon)=>{
+    icon.classList.remove('selected');
+  });
+  stopConfetti();
+  removeConfetti();
+}
+
+function resetAll(){
+  playerScore=0;
+  computerScore=0;
+  playerScoreEl.textContent = playerScore;
+  computerScoreEl.textContent = computerScore;
+  playerChoiceEl.textContent='';
+  computerChoiceEl.textContent='';
+  resultText.textContent='';
+  resetSelected();
+
+}
+window.resetAll = resetAll;
+
+function computerRandomChoice(){
+  const computerChoiceNumber = Math.random();
+  if(computerChoiceNumber <0.2){
+    computerChoice='rock';
+  }else if(computerChoiceNumber <=0.4){
+    computerChoice ='paper';
+  }else if(computerChoiceNumber <=0.6){
+    computerChoice = 'scissors';
+  }else if(computerChoiceNumber <=0.8){
+    computerChoice = 'lizard';
+  }else{
+    computerChoice = 'spock';
+  }
+  
+}
+
+function displayComputerChoice(){
+  
+  switch(computerChoice){
+    case 'rock':
+     computerRock.classList.add('selected');
+     computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+     computerPaper.classList.add('selected');
+     computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+       computerScissors.classList.add('selected');
+       computerChoiceEl.textContent = ' --- Scissors';
+        break;
+    case 'lizard':
+       computerLizard.classList.add('selected');
+       computerChoiceEl.textContent = ' --- Lizard';
+        break;
+    case 'spock':
+       computerSpock.classList.add('selected');
+       computerChoiceEl.textContent = ' --- Spock';
+      break;
+    default:
+      break;
+  }
+}
+
+function updateScore(playerChoice){
+    if(playerChoice === computerChoice){
+      resultText.textContent = "It's a tie.";
+    } else{
+      const choice = choices[playerChoice];
+      if(choice.defeats.indexOf(computerChoice)>=0){
+        startConfetti();
+        resultText.textContent = "You Won!";
+         playerScore++;
+         playerScoreEl.textContent=playerScore;
+      }else{
+        
+        resultText.textContent = "You Lost!";
+        computerScore++;
+        computerScoreEl.textContent=computerScore;
+      }
+    }
+}
+
+function checkResult(playerChoice){
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+  updateScore(playerChoice)
+}
 
 function select(playerChoice){
-  console.log(playerChoice);
+  checkResult(playerChoice);
+  switch(playerChoice){
+    case 'rock':
+      playerRock.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      playerPaper.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+        playerScissors.classList.add('selected');
+        playerChoiceEl.textContent = ' --- Scissors';
+        break;
+    case 'lizard':
+        playerLizard.classList.add('selected');
+        playerChoiceEl.textContent = ' --- Lizard';
+        break;
+    case 'spock':
+        playerSpock.classList.add('selected');
+        playerChoiceEl.textContent = ' --- Spock';
+      break;
+    default:
+      break;
+  }
 }
+ 
+window.select = select;
+
+resetAll();
